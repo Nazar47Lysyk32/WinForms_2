b3bf388cc5ef2247a321671877220bf99fc2132a
Nazar47Lysyk32 <137522819+Nazar47Lysyk32@users.noreply.github.com>
2024-05-07 14:27:22 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAddTask = new System.Windows.Forms.Button();
+            this.btnRemoveTask = new System.Windows.Forms.Button();
+            this.btnMarkAsDone = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // listBox1
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 186);
+            this.listBox1.TabIndex = 0;
+
+            // textBox1
+            this.textBox1.Location = new System.Drawing.Point(12, 204);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 20);
+            this.textBox1.TabIndex = 1;
+
+            // btnAddTask
+            this.btnAddTask.Location = new System.Drawing.Point(196, 202);
+            this.btnAddTask.Name = "btnAddTask";
+            this.btnAddTask.Size = new System.Drawing.Size(75, 23);
+            this.btnAddTask.TabIndex = 2;
+            this.btnAddTask.Text = "Add Task";
+            this.btnAddTask.UseVisualStyleBackColor = true;
+            this.btnAddTask.Click += new System.EventHandler(this.btnAddTask_Click);
+
+            // btnRemoveTask
+            this.btnRemoveTask.Location = new System.Drawing.Point(12, 230);
+            this.btnRemoveTask.Name = "btnRemoveTask";
+            this.btnRemoveTask.Size = new System.Drawing.Size(94, 23);
+            this.btnRemoveTask.TabIndex = 3;
+            this.btnRemoveTask.Text = "Remove Task";
+            this.btnRemoveTask.UseVisualStyleBackColor = true;
+            this.btnRemoveTask.Click += new System.EventHandler(this.btnRemoveTask_Click);
+
+            // btnMarkAsDone
+            this.btnMarkAsDone.Location = new System.Drawing.Point(112, 230);
+            this.btnMarkAsDone.Name = "btnMarkAsDone";
+            this.btnMarkAsDone.Size = new System.Drawing.Size(94, 23);
+            this.btnMarkAsDone.TabIndex = 4;
+            this.btnMarkAsDone.Text = "Mark as Done";
+            this.btnMarkAsDone.UseVisualStyleBackColor = true;
+            this.btnMarkAsDone.Click += new System.EventHandler(this.btnMarkAsDone_Click);
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnMarkAsDone);
+            this.Controls.Add(this.btnRemoveTask);
+            this.Controls.Add(this.btnAddTask);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Todo List App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAddTask;
+        private System.Windows.Forms.Button btnRemoveTask;
+        private System.Windows.Forms.Button btnMarkAsDone;
+    }
+
+
+
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddTask_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                listBox1.Items.Add(textBox1.Text);
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter some text for the task.");
+            }
+        }
+
+        private void btnRemoveTask_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to remove.");
+            }
+        }
+
+        private void btnMarkAsDone_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox1.SelectedIndex;
+            if (selectedIndex != -1)
+            {
+                listBox1.Items[selectedIndex] = "[Done] " + listBox1.Items[selectedIndex];
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to mark as completed.");
+            }
+        }
+    }
+
+}
